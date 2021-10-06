576734810ed555ce73a6e64ff461eec6645d7468
NDThuan99 <nguyenducthuan831999@gmail.com>
2021-10-06 10:51:51 +0700

    Initial commit

--- app/src/main/java/com/example/scrollpositionrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/scrollpositionrecyclerview/MainActivity.java
@@ -6,6 +6,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+<<<<<<< HEAD
+=======
+import android.widget.LinearLayout;
+>>>>>>> 61adeaf (Initial commit)
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -22,10 +26,19 @@ public class MainActivity extends AppCompatActivity {
         int position = 22;
         setArr();
         DongAdapter dongAdapter = new DongAdapter(arr);
+<<<<<<< HEAD
         rcvList.setLayoutManager(new LinearLayoutManager(this));
         rcvList.setAdapter(dongAdapter);
         rcvList.scrollToPosition(position);
         //scroll tới vị trí của vị trí đang được chọn
+=======
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        rcvList.setLayoutManager(manager);
+        rcvList.setAdapter(dongAdapter);
+        //scroll tới vị trí của vị trí đang được chọn
+        //rcvList.scrollToPosition(position);
+        manager.scrollToPositionWithOffset(position, arr.size());
+>>>>>>> 61adeaf (Initial commit)
         txtPosition.setText("dòng được scroll là"+ position);
     }
     private void setArr(){
